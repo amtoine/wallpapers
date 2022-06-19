44d254298d1b65600844f94162d7c20db1d191b4
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2022-06-19 13:28:33 +0200

    (refactor) remove the montage
    montage.png was useless with the per-file preview.

--- generate_preview.sh
+++ generate_preview.sh
@@ -5,10 +5,6 @@
 nb_note="""
 A gallery of wallpapers I use for my laptop config.
 
-| ![montage.png](montage.png) |
-|:--:|
-| ***Overview of the wallpapers inside the repo.** Images have been stretched to fit inside squares.* |
-
 ## Ownership.
 These wallpapers come from the internet. Diffusion and usage rights are not known for all of them.
 **I do not own any of the wallpapers listed in this repo.**
